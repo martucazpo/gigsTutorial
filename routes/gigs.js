2e75b53576e12cdcbb3069ae8641a56efbdd7651
martucazpo <martucazpo@gmail.com>
2020-09-27 20:35:15 -0700

    pt 2 handlebars err work around, css image workaround

--- routes/gigs.js
+++ routes/gigs.js
@@ -3,10 +3,53 @@ const db = require('../config/database');
 const Gig = require('../models/Gig');
 
 router.get('/', (req, res) => Gig.findAll()
-.then(gigs => {
-    console.log(gigs);
-    res.sendStatus(200);
-})
-.catch(err => console.log(err)));
+    .then(gigsObject => {
+        const newGigsObject = {
+            gigs: gigsObject.map(data => {
+                return {
+                    title: data.title,
+                    description: data.description,
+                    technologies: data.technologies,
+                    budget: data.budget,
+                    contact_email: data.contact_email
+                };
+            })
+        };
+        res.render('gigs', {
+            gigs: newGigsObject.gigs
+        });
+    })
+    .catch(err => console.log(err)));
+
+
+router.get('/add', (req, res) => res.render('add'));
+
+router.post('/add', (req, res) => {
+    const data = {
+        title: 'simple Wordpress website',
+        technologies: 'wordpress,php,html,css',
+        budget: '$1000',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Et malesuada fames ac turpis egestas maecenas. Ac turpis egestas sed tempus urna et pharetra. Est ullamcorper eget nulla facilisi etiam dignissim diam quis enim. Urna nec tincidunt praesent semper feugiat nibh.',
+        contact_email: 'user2@gmail.com'
+    }
+
+    let {
+        title,
+        technologies,
+        budget,
+        description,
+        contact_email
+    } = data;
+
+    Gig.create({
+            title,
+            technologies,
+            budget,
+            description,
+            contact_email
+        })
+        .then(gig => res.redirect('/gigs'))
+        .catch(err => console.log(err));
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,16 +1,23 @@
 const express = require('express');
+const path = require('path')
 const exphbs = require('express-handlebars');
 const app = express();
 const gigRoutes = require('./routes');
 const PORT = process.env.PORT || 3000;
 const db = require('./config/database');
 
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
 
 db.authenticate()
 .then(() => console.log('database connected'))
 .catch(err => console.log(err));
 
-app.get('/', (req, res) => res.send('Index'));
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+app.use(express.static('public'));
+ 
+app.get('/', (req, res) => res.render('Index', { layout: 'landing' }));
 app.use('/gigs', gigRoutes);
 
 app.listen(PORT, console.log(`Server listening on port ${PORT}`));
