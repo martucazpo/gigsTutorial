e7a7053ae3030e070c3b4fafb025592a21af4338
martucazpo <martucazpo@gmail.com>
2020-09-28 16:31:24 -0700

    search added

--- routes/gigs.js
+++ routes/gigs.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
-const db = require('../config/database');
+//const db = require('../config/database');
 const Gig = require('../models/Gig');
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
 
 router.get('/', (req, res) => Gig.findAll()
     .then(gigsObject => {
@@ -80,6 +82,29 @@ router.post('/add', (req, res) => {
             .then(gig => res.redirect('/gigs'))
             .catch(err => console.log(err));
     }
+});
+
+router.get('/search', (req, res) => {
+    let { term } = req.query;
+    term = term.toLowerCase();
+    Gig.findAll({ where: { technologies: { [Op.like] : '%' + term + '%' } }})
+    .then(gigsObject => {
+        const newGigsObject = {
+            gigs: gigsObject.map(data => {
+                return {
+                    title: data.title,
+                    description: data.description,
+                    technologies: data.technologies,
+                    budget: data.budget,
+                    contact_email: data.contact_email
+                };
+            })
+        };
+        res.render('gigs', {
+            gigs: newGigsObject.gigs
+        });
+    })
+    .catch( err => console.log(err) );
 });
 
 module.exports = router;
